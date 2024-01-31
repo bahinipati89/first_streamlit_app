dcce2246dedf134db26caa8bf13e4211483d7515
bahinipati89 <158331587+bahinipati89@users.noreply.github.com>
2024-02-01 00:17:05 +0530

    Update streamlit_app.py
    Added header
    added few text for food items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title('My Parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Sandwiches')
+streamlit.text('Poha')
+streamlit.text('Upma')
+streamlit.text('Masala Dosa')
+streamlit.text('Omelette')
+streamlit.text('Vanila Smoothie')
